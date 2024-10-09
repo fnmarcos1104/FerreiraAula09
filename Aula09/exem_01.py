090c14a2c1d762a74582c5129587953a21de52c9
fnmarcos1104 <marcosfnf@hotmail.com>
2024-10-08 21:07:08 -0300

    Update1 no arquivo exem_01.py

--- Aula09/exem_01.py
+++ Aula09/exem_01.py
@@ -2,10 +2,10 @@ import os
 
 os.system('cls')
 
-try:
-    n = int(input('Informe um número: '))
-except:
-    print(f'Erro: O usuário não digitou um número!')
+# try:
+#     n = int(input('Informe um número: '))
+# except:
+#     print(f'Erro: O usuário não digitou um número!')
 
 
 # try:
@@ -15,3 +15,23 @@ except:
 # except KeyboardInterrupt:
 #     print(f'\nErro: O usuário cancelou a operação!')
 
+
+
+try:
+    n = int(input('Informe um número: '))
+except (ValueError, KeyboardInterrupt) as e:
+    print(f'Erro: O usuário não digitou um número!\nErro: {e}')
+else:
+    print(f'Você informou: {n}')
+
+
+
+
+# try:
+#     txt = input('Informe um nome: ')[0]
+# except IndexError as e:
+#     print(f'Erro: É necessário digitar algo!\nError: {e}')
+# else:
+#     print('OK!')
+# finally:
+#     print('Fim')
