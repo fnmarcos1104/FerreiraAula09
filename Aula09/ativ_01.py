e474626aa38ef70cb639a22cbf4a3c2728b814e8
fnmarcos1104 <marcosfnf@hotmail.com>
2024-10-08 21:26:14 -0300

    inicio ativ_01.py

---
+++ Aula09/ativ_01.py
@@ -0,0 +1,15 @@
+import os
+
+os.system('cls')
+
+while True:
+    try:
+        num = int(input('Informe um número: '))
+    except(ValueError) as e:
+        print(f'{e} Valor informado não é válido')
+    except KeyboardInterrupt as e:
+        print(f'{e} Entrada interrompida pelo usário')
+        break
+    else:
+        print(f'Número {num} informado com sucesso')
+        break
